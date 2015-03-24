dba252a2d8e7a92dc36e7fa63cdb3142562d24a4
Tracy Brandt Jr <leebrandt@smaug.us.fortis.com>
2015-03-24 13:11:23 -0500

    E2E conf for protractor

--- tests/e2e/conf.js
+++ tests/e2e/conf.js
@@ -4,22 +4,23 @@ exports.config = {
   specs: ['*.js'],
   capabilities: {
     browserName: 'phantomjs',
-	  /* 
-	   * Can be used to specify the phantomjs binary path.
-	   * This can generally be ommitted if you installed phantomjs globally.
-	   */
+
+	  // Can be used to specify the phantomjs binary path.
+	  // This can generally be ommitted if you installed phantomjs globally.
 	  'phantomjs.binary.path': require('phantomjs').path,
 
-	  /*
-	   * Command line args to pass to ghostdriver, phantomjs's browser driver.
-	   * See https://github.com/detro/ghostdriver#faq
-	   */
+	  // Command line args to pass to ghostdriver, phantomjs's browser driver.
+	  // See https://github.com/detro/ghostdriver#faq
 	  'phantomjs.ghostdriver.cli.args': ['--loglevel=DEBUG']
 	},
+	
 	// A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:3000',
+  
   framework: 'jasmine',
+
+  // onPrepare happens when preparing to run the tests
   onPrepare: function() {
 	  beforeEach(function() {
 	    // things to do before every run
