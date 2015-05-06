80b38e227febd2acdf3b176aa37a338155a08c51
Lee Brandt <leebrandt@gmail.com>
2015-05-06 11:51:14 -0500

    Activating, Deactivating, Locking and Unlocking users stubs added.

--- src/modules/user/controllers/user.controller.js
+++ src/modules/user/controllers/user.controller.js
@@ -17,6 +17,22 @@
 			});
 		};
 
+		this.activateUser = function(user){
+			console.log('Activating:', user);
+		};
+
+		this.deactivateUser = function(user){
+			console.log('Deactivating:', user);
+		};
+
+		this.lockUser = function(user){
+			console.log('Locking:', user);
+		};
+
+		this.unlockUser = function(user){
+			console.log('Unlocking:', user);
+		};
+
 		this.saveUser = function(){
 			console.log('saving', ctrl.user);
 		};
